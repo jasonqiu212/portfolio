f091ce508330607919865fccc3601a42c1703257
jasonqiu212 <jason_qiu@hotmail.com>
2025-01-29 14:43:15 +0800

    feat: add mobile menu

---
+++ src/components/ui/NavBar/NavBar.tsx
@@ -0,0 +1,105 @@
+import {
+  Burger,
+  Container,
+  Group,
+  Menu,
+  Switch,
+  Title,
+  useComputedColorScheme,
+  useMantineColorScheme,
+} from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
+import { IconMoonFilled, IconSunFilled } from '@tabler/icons-react';
+
+import UnderlinedButton from '../UnderlinedButton/UnderlinedButton';
+import classes from './NavBar.module.css';
+
+const links = [
+  {
+    label: 'about me',
+  },
+  {
+    label: 'skills',
+  },
+  {
+    label: 'experience',
+  },
+  {
+    label: 'projects',
+  },
+];
+
+function NavBar() {
+  const { setColorScheme } = useMantineColorScheme({ keepTransitions: true });
+  const computedColorScheme = useComputedColorScheme();
+  const isLightMode = computedColorScheme === 'light';
+
+  const [isMenuOpened, { toggle: toggleMenu }] = useDisclosure();
+
+  return (
+    <Container
+      pos="fixed"
+      w="100%"
+      h="70px"
+      px={{ base: '6vw', lg: '10vw' }}
+      pt="20px"
+      maw="unset"
+      style={{
+        alignContent: 'center',
+        backdropFilter: 'blur(10px)',
+        zIndex: 2,
+      }}
+    >
+      <Group justify="space-between">
+        <Title variant="default">Jason Qiu</Title>
+        <Group gap="lg">
+          <Group gap="lg" display={{ base: 'none', lg: 'flex' }}>
+            {links.map((link) => (
+              <UnderlinedButton label={link.label} />
+            ))}
+          </Group>
+
+          <Switch
+            checked={!isLightMode}
+            onChange={() => setColorScheme(isLightMode ? 'dark' : 'light')}
+            size="lg"
+            color="slate.7"
+            onLabel={
+              <IconMoonFilled size={22} color="var(--mantine-color-indigo-4)" />
+            }
+            offLabel={
+              <IconSunFilled size={22} color="var(--mantine-color-red-6)" />
+            }
+          />
+
+          <Menu
+            opened={isMenuOpened}
+            onChange={toggleMenu}
+            position="bottom-end"
+            classNames={{
+              dropdown: classes.dropdown,
+              item: classes.item,
+              itemLabel: classes.itemLabel,
+            }}
+          >
+            <Menu.Target>
+              <Burger
+                opened={isMenuOpened}
+                onClick={toggleMenu}
+                display={{ base: 'inline-block', lg: 'none' }}
+                aria-label="Toggle menu"
+              />
+            </Menu.Target>
+            <Menu.Dropdown>
+              {links.map((link) => (
+                <Menu.Item>{link.label}</Menu.Item>
+              ))}
+            </Menu.Dropdown>
+          </Menu>
+        </Group>
+      </Group>
+    </Container>
+  );
+}
+
+export default NavBar;
