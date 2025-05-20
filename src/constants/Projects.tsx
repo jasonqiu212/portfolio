d17b6c56f440dd07fa23576162075df65ad8eba5
jasonqiu212 <jason_qiu@hotmail.com>
2025-05-20 21:31:31 +0800

    feat: update project descriptions

--- src/constants/Projects.tsx
+++ src/constants/Projects.tsx
@@ -8,8 +8,8 @@ export const projects = [
     name: 'have anot?',
     url: 'https://www.build.gov.sg/have-anot/',
     descriptions: [
-      'A chatbot that helps public officers to solve problems by recommending existing digital products. It enables public officers to minimize the need for completely bespoke systems, saving both cost and time.',
-      "have anot? was created for Open Government Products's Build for Good 2024 hackathon.",
+      'A chatbot that helps public officers to solve problems by recommending existing digital products. It enables public officers to minimize the need for bespoke systems, saving both cost and time.',
+      "Created for Open Government Products's Build for Good 2024.",
     ],
     techStack: [
       icons['typescript'],
@@ -18,7 +18,12 @@ export const projects = [
       icons['langchain'],
       icons['git'],
     ],
-    links: [],
+    links: [
+      {
+        label: 'View the code on GitHub',
+        url: 'https://github.com/jasonqiu212/HaveAnot',
+      },
+    ],
     imageUrl: HaveAnot,
   },
   {
@@ -26,6 +31,7 @@ export const projects = [
     url: 'https://teammatesv4.appspot.com/web/front/home',
     descriptions: [
       'An open source peer evaluation website used by over 1 million users and 1,100 universities.',
+      'As a project maintainer, I review pull requests and triage bug reports from external contributors. I previously enhanced web accessibility and mobile responsiveness, and co-led the migration from Google Datastore to SQL.',
     ],
     techStack: [
       icons['typescript'],
@@ -47,7 +53,8 @@ export const projects = [
     name: 'PeerPrep',
     url: 'https://peerprep.zoeang.dev/',
     descriptions: [
-      'A platform for programmers to practice technical interviews by matching with others to practice LeetCode questions. Features include collaborative coding pad, code execution, video calling, and storage of practice attempts.',
+      'A platform for programmers to practice technical interviews by matching with others to practice LeetCode questions. Features include collaborative coding pad, code execution, video calling, and practice history.',
+      'Built this app as the frontend developer on a team of five.',
     ],
     techStack: [
       icons['typescript'],
